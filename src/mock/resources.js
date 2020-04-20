181c857e60bfb311e12854bc4d33c785f576174b
hellojaliyah <jaliyahdharrison@gmail.com>
2020-04-20 16:28:22 -0400

    Created mock resource file and dynamically displayed resources to app.

---
+++ src/mock/resources.js
@@ -0,0 +1,42 @@
+const resources = [
+    {
+      posterName: 'Cliff',
+      resourceAuthor: 'NetNinja',
+      authorSkillLevel: 'Advanced',
+      cohort: '0',
+      title: 'React & Redux Complete',
+      categories: ['React', 'Redux'],
+      summary: 'A good resource on building a couple react apps with and without redux',
+      link: 'www.google.com/search?q=NetNinja%20React',
+      resourceType: 'Playlist',
+      datePublished: '2018-09-10.00:00:00.000z',
+      videoLength: null,
+      timeToComplete: 300,
+      rating: 5,
+      comments: [
+        {user: 'Joe', text:'Pretty good research'},
+        {user: 'Phen', text: 'The best to learn React with.'}
+      ]
+    },
+    {
+      posterName: "John F",
+      resourceAuthor: "DevEd",
+      authorSkillLevel: "Beginner",
+      cohort: "8",
+      title: "React Tutorial For Beginners",
+      categories: ["React", "Beginner"],
+      summary: "A beginner's guide to React, React State Management, React Router, and Redux. Include 5 app projects.",
+      link: "https://www.youtube.com/playlist?list=PLDyQo7g0_nsVHmyZZpVJyFn5ojlboVEhE",
+      resourceType: "Playlist",
+      datePublished: "2019-06-22.00:00:00.000z",
+      videoLength: null,
+      timeToComplete: 200,
+      rating: 4,
+      comments: [
+        { user: "Tony", text: "That crazy Romanian knows his stuff!" },
+        { user: "Sheila", text: "A good intro to React." },
+      ]
+    }  
+];
+
+export default resources;
